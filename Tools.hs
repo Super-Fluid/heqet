3549bb7b28bf842dec6819cd3c26a8b07474e476
Isaac Reilly <isaac.g.reilly@gmail.com>
2015-10-29 12:13:05 -0400

    Tools WIP: startMusicAt time done, types for take/drop and reverse

--- Tools.hs
+++ Tools.hs
@@ -6,3 +6,17 @@ import Control.Lens
 
 mapOverNotes :: (Note a -> Note a) -> MusicOf a -> MusicOf a
 mapOverNotes = map . fmap
+
+startMusicAt :: PointInTime -> MusicOf a -> MusicOf a
+startMusicAt pt mus = fst $ foldl f ([],pt) mus where
+    f (acc, pt) inT = ((inT & t .~ pt):acc, pt+(inT^.dur))
+
+startMusicAtZero :: MusicOf a -> MusicOf a
+startMusicAtZero = startMusicAt 0
+
+--takeMusic :: PointInTime -> MusicOf a -> MusicOf a
+--takeMusic t mus = 
+
+--dropMusic :: PointInTime -> MusicOf a -> MusicOf a
+
+--reverseMusic :: MusicOf a -> MusicOf a
