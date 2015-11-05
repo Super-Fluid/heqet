c5d0eb135ece55fb1389337cfab8969d6fb97985
Isaac Reilly <isaac.g.reilly@gmail.com>
2015-11-04 21:16:53 -0500

    add some tools for selecting music (wip..)

--- Tools.hs
+++ Tools.hs
@@ -5,6 +5,8 @@ import List
 
 import Control.Lens
 import Data.List
+import Control.Applicative
+import Data.Maybe (mapMaybe)
 
 mapOverNotes :: (Note a -> Note a) -> MusicOf a -> MusicOf a
 mapOverNotes = map . fmap
@@ -19,12 +21,23 @@ startMusicAtZero = startMusicAt 0
 voice :: String -> Lens' Music Music
 voice v = filteringBy (\it -> lookup "voice" (it^.val.tags) == Just v)
 
+instName :: String -> Lens' Music Music
+instName v = filteringBy (\it -> ((^.name) <$> it^.val.inst) == Just v)
+
+instKind :: String -> Lens' Music Music
+instKind v = filteringBy (\it -> ((^.kind) <$> it^.val.inst) == Just v)
+
 timeSort :: [InTime a] -> [InTime a]
 timeSort = sortBy $ \it1 it2 -> (it1^.t) `compare` (it2^.t) 
 
---takeMusic :: PointInTime -> MusicOf a -> MusicOf a
---takeMusic t mus = 
-
+{-
+takeMusic :: PointInTime -> Lens' Music Music
+takeMusic pit mus = lens (mapMaybe f) (\s a -> a++s) where
+    f it
+        | it^.t >= pit = Nothing
+        | it^.t + it^.dur <= pit = Just it
+        | otherwise = Just $ it & dur .~ (pit - it^.t) & val.isTied .~ True
+-}
 --dropMusic :: PointInTime -> MusicOf a -> MusicOf a
 
 --reverseMusic :: MusicOf a -> MusicOf a
