a37ee8651a347784271feb3fec22479af33b10eb
Isaac Reilly <isaac.g.reilly@gmail.com>
2015-11-11 18:37:32 -0500

    refactor test cases for better naming

--- TestCases.hs
+++ TestCases.hs
@@ -9,21 +9,20 @@ import Types
 
 import Control.Lens
 
-linear1 = [music| c'4. df'8-. b2\fermata\with"^\"long\"" |]
-linear2 = linear1 & mapOverNotes (\x -> x
+
+linear = [music| c'4. df'8-. b2\fermata\with"^\"long\"" |] & mapOverNotes (\x -> x
     & clef .~ Just Treble
     & line .~ Just "1"
     & inst .~ Just Instruments.melody
     )
 
-chordal1 = [music| <c e>4. <b d fs>8-! |]
-chordal1' = chordal1 & mapOverNotes (\x -> x
+chordal = [music| <c e>4. <b d fs>8-! |] & mapOverNotes (\x -> x
     & clef .~ Just Treble
     & line .~ Just "1"
     & inst .~ Just Instruments.melody
     )
 
-poly1 = [music| f1 << { c4 g, } \\ { e2 } >> <g, b>2 c1\fermata |] 
+poly = [music| f1 << { c4 g, } \\ { e2 } >> <g, b>2 c1\fermata |] 
     & mapOverNotes (\x -> x
         & clef .~ Just Treble
         & line .~ Just "1"
