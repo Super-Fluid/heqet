a4842b472c423bfd4dd7af98456870f42a042552
Isaac Reilly <isaac.g.reilly@gmail.com>
2015-11-12 13:29:47 -0500

    render: if music has multiple lines, print them simultaineously

--- Output/Templates.hs
+++ Output/Templates.hs
@@ -1,8 +1,8 @@
 module Output.Templates where
 
 -- operating on music:
-basicBeginScore = "\\version \"2.16.2\"\n\\language \"english\"\n\\score {\n"
-basicEndScore = "\\layout { }\n\\midi { }\n}"
+basicBeginScore = "\\version \"2.16.2\"\n\\language \"english\"\n\\score { <<\n"
+basicEndScore = ">> \n\\layout { }\n\\midi { }\n}"
 
 basicBeginStaff = "\\new Staff \\with {\nmidiInstrument = \"bassoon\"\n} { \n\\once \\override Staff.TimeSignature #'stencil = ##f \n\\clef bass\n\\cadenzaOn "
 basicEndStaff = "\n\\cadenzaOff\n \\bar \"|\"\n}\n"
