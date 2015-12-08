0390d85c7a913c91d8f46dc4dc16d7e6b91f8da7
Isaac Reilly <isaac.g.reilly@gmail.com>
2015-12-07 19:46:17 -0500

    revise meter names: m34 -> m3_4 and add meter lookup table

--- Meters.hs
+++ Meters.hs
@@ -1,13 +1,13 @@
 module Meters 
 (
-m44,
-m22,
-m34,
-m54,
-m38,
-m68,
-m98,
-m128,
+m4_4,
+m2_2,
+m3_4,
+m5_4,
+m3_8,
+m6_8,
+m9_8,
+m12_8,
 commonTime,
 cutTime,
 assignMeter
@@ -16,7 +16,6 @@ assignMeter
 where
 
 import Types
-import Output.Render -- for the Renderable instances
 import Tools
 
 import Control.Lens
@@ -28,44 +27,61 @@ beat :: Music
 beat = [InTime { _t=0, _dur=0, _val= emptyNote & pitch .~ Ly LyBeatEvent }]
 
 -- some shortcuts:
-beat2 = startMusicAt 1 beat
-beat2dot = startMusicAt (3/2) beat
-beat3 = startMusicAt 2 beat
-beat3dot = beat4
-beat4 = startMusicAt 3 beat
-beat5 = startMusicAt 4 beat
-beat4dot = startMusicAt (9/2) beat
+beatAt :: Duration -> Music
+beatAt d = startMusicAt d beat
+
+beat2 = beatAt (1/4)
+beat3 = beatAt (2/4)
+beat4 = beatAt (3/4)
+beat5 = beatAt (4/4)
 
 type MMeter = (Music,Duration)
 
-m44 :: MMeter
-m44 = (concat [measure,beat,beat2,beat3,beat4], 4)
-commonTime = m44
+m4_4 :: MMeter
+m4_4 = (concat [measure,beat,beat2,beat3,beat4], 4/4)
+commonTime = m4_4
 
-m22 :: MMeter
-m22 = (concat [measure,beat,beat3], 4)
-cutTime = m22
+m2_2 :: MMeter
+m2_2 = (concat [measure,beat,beat3], 2/2)
+cutTime = m2_2
 
-m34 :: MMeter
-m34 = (concat [measure,beat,beat2,beat3], 3)
+m3_4 :: MMeter
+m3_4 = (concat [measure,beat,beat2,beat3], 3/4)
 
-m24 :: MMeter
-m24 = (concat [measure,beat,beat2], 2)
+m2_4 :: MMeter
+m2_4 = (concat [measure,beat,beat2], 2/4)
 
-m54 :: MMeter
-m54 = (concat [measure,beat,beat2,beat3,beat4,beat5], 5)
+m5_4 :: MMeter
+m5_4 = (concat [measure,beat,beat2,beat3,beat4,beat5], 5/4)
 
-m38 :: MMeter
-m38 = (concat [measure,beat], 3/2)
+m3_8 :: MMeter
+m3_8 = (concat [measure,beat], 3/8)
 
-m68 :: MMeter
-m68 = (concat [measure,beat,beat2dot], 3)
+m6_8 :: MMeter
+m6_8 = (concat [measure,beat,beatAt $ 3/8], 6/8)
 
-m98 :: MMeter
-m98 = (concat [measure,beat,beat2dot,beat3dot], 9/2)
+m9_8 :: MMeter
+m9_8 = (concat [measure,beat,beatAt $ 3/8,beatAt $ 6/8], 9/8)
 
-m128 :: MMeter
-m128 = (concat [measure,beat,beat2dot,beat3dot,beat4dot], 6)
+m12_8 :: MMeter
+m12_8 = (concat [measure,beat,beatAt $ 3/8,beatAt $ 6/8,beatAt $ 9/8], 12/8)
+
+-- 5/8 possibilities
+m3'2_8 :: MMeter
+m3'2_8 = (concat [measure,beat,beatAt $ 3/8], 5/8)
+
+m2'3_8 :: MMeter
+m2'3_8 = (concat [measure,beat,beat2], 5/8)
+
+-- 7/8 possibilities
+m3'3'2_8 :: MMeter
+m3'3'2_8 = (concat [measure,beat,beatAt $ 3/8,beatAt $ 6/8], 7/8)
+
+m3'2'3_8 :: MMeter
+m3'2'3_8 = (concat [measure,beat,beatAt $ 3/8,beatAt $ 5/8], 7/8)
+
+m2'3'3_8 :: MMeter
+m2'3'3_8 = (concat [measure,beat,beatAt $ 2/8,beatAt $ 5/8], 7/8)
 
 assignMeter :: MMeter -> Music -> Music
 assignMeter = assignMeterWithLine "all"
@@ -79,3 +95,32 @@ assignMeterWithLine s (rawmeter,duration) music = music ++ (meterTrack $ getStar
         if p >= endTime
         then []
         else meter ++ meterTrack (p+duration)
+
+-- to render changes in meter, we need to go from measure and beat
+-- events to Meters
+
+type MeterSignature = ([PointInTime],Duration)
+
+getMeterSignature :: MMeter -> MeterSignature
+getMeterSignature (m, d) = let
+    beats = m^.ofType (lyBeatEventType)
+    in (beats^..traverse.t,d)
+
+meterTable :: [(MeterSignature,LyMeterEvent)]
+meterTable = [
+     (getMeterSignature m4_4, LyMeterEvent $ Meter 4 4 )
+    ,(getMeterSignature m2_2, LyMeterEvent $ Meter 2 2 )
+    ,(getMeterSignature m2_4, LyMeterEvent $ Meter 2 4 )
+    ,(getMeterSignature m3_4, LyMeterEvent $ Meter 3 4 )
+    ,(getMeterSignature m5_4, LyMeterEvent $ Meter 5 4 )
+    ,(getMeterSignature m3_8, LyMeterEvent $ Meter 3 8 )
+    ,(getMeterSignature m6_8, LyMeterEvent $ Meter 6 8 )
+    ,(getMeterSignature m9_8, LyMeterEvent $ Meter 9 8 )
+    ,(getMeterSignature m12_8, LyMeterEvent $ Meter 12 8 )
+    ,(getMeterSignature m2'3_8, LyMeterEvent $ Meter 5 8 )
+    ,(getMeterSignature m3'2_8, LyMeterEvent $ Meter 5 8 )
+    ,(getMeterSignature m3'3'2_8, LyMeterEvent $ Meter 7 8 )
+    ,(getMeterSignature m3'2'3_8, LyMeterEvent $ Meter 7 8 )
+    ,(getMeterSignature m2'3'3_8, LyMeterEvent $ Meter 7 8 )
+    ]
+
