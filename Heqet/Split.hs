5d7415aaf2321ac876d8ab6b13411d01715c1d89
Isaac Reilly <isaac.g.reilly@gmail.com>
2015-12-16 21:41:38 -0500

    add splitting at any note you want

--- Heqet/Split.hs
+++ Heqet/Split.hs
@@ -8,10 +8,13 @@ import Heqet.Input.English
 import Control.Lens
 
 superBasicSplit :: Music -> Music
-superBasicSplit = concatMap (\n ->
+superBasicSplit = superBasicSplitAt [pp| c' |]
+
+superBasicSplitAt :: Pitch -> Music -> Music
+superBasicSplitAt splitPitch = concatMap (\n ->
     if isPlayable (n^.val.pitch)
     then 
-        if getNoteHeight n == Nothing || getNoteHeight n > getPitchHeight [pp| c' |]
+        if getNoteHeight n == Nothing || getNoteHeight n > getPitchHeight splitPitch
         then [n & val.subStaff .~ Just RH]
         else [n & val.subStaff .~ Just LH]
     else [n & val.subStaff .~ Just RH, n & val.subStaff .~ Just LH] -- put on both events
